35d79add2a279b1a383bd1949d52c31d289c3f9a
doanhungff123123-commits <doanhungff123123@gmail.com>
2026-02-05 20:35:11 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,18 +1,11 @@
 if getgenv().HungDaoFlyLoaded then 
-    warn("⚠️ Script already loaded! Please reset to reload.")
+    warn("Script already loaded!")
     return 
 end
 
 getgenv().HungDaoFlyLoaded = true
 
-local success, err = pcall(function()
-    loadstring(game:HttpGet(
-        "https://raw.githubusercontent.com/doanhungf123123/commits/main/script.lua",
-        true
-    ))()
-end)
-
-if not success then
-    warn("❌ Failed to load script: " .. tostring(err))
-    getgenv().HungDaoFlyLoaded = nil -- Reset nếu lỗi
-end
+loadstring(game:HttpGet(
+    "https://raw.githubusercontent.com/doanhungf123123/commits/main/script.lua",
+    true
+))()
