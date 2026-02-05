82aab9377a37b35472687df4f6de3e4aca6667a3
doanhungff123123-commits <doanhungff123123@gmail.com>
2026-02-05 20:20:05 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,14 +1,4 @@
 local url = "https://raw.githubusercontent.com/doanhungf123123/commits/main/script.lua"
-local success, err = pcall(function()
-	local code = game:HttpGet(url)
-	local func = loadstring(code)
-	if func then
-		func()()
-		print("Loaded")
-	else
-		warn("Load failed")
-	end
+pcall(function()
+	loadstring(game:HttpGet(url))()()
 end)
-if not success then
-	warn("Error: " .. tostring(err))
-end
