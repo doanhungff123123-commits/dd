5ae01083eeef8fd9137bfce17dc958308469fc9e
doanhungff123123-commits <doanhungff123123@gmail.com>
2026-02-05 20:37:36 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,11 +1,20 @@
 if getgenv().HungDaoFlyLoaded then 
-    warn("Script already loaded!")
+    print("Script already running!")
     return 
 end
 
 getgenv().HungDaoFlyLoaded = true
 
-loadstring(game:HttpGet(
-    "https://raw.githubusercontent.com/doanhungf123123/commits/main/script.lua",
-    true
-))()
+local success, err = pcall(function()
+    local scriptFunc = loadstring(game:HttpGet("https://raw.githubusercontent.com/doanhungf123123/commits/main/script.lua", true))
+    if scriptFunc then
+        scriptFunc()
+    else
+        error("Failed to load script from GitHub")
+    end
+end)
+
+if not success then
+    warn("Error loading script: " .. tostring(err))
+    getgenv().HungDaoFlyLoaded = nil
+end
